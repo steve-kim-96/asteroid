a0b9289fecf85e6f6978ce5a39789a5a3e466bd1
steve-kim-96 <soungku@hotmail.com>
2025-03-20 22:28:52 +1300

    use player class to create player sprite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from constants import ASTEROID_KINDS, ASTEROID_MAX_RADIUS, ASTEROID_MIN_RADIUS, ASTEROID_SPAWN_RATE, SCREEN_HEIGHT, SCREEN_WIDTH
 import pygame
+from player import Player
 
 def main():
     pygame.init()
@@ -8,12 +9,25 @@ def main():
     print("Screen width: 1280")
     print("Screen height: 720")
 
+    fps_clock = pygame.time.Clock()
+    dt = 0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+
+    player = Player(x, y)
+
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen,(0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+
+        time_passed = fps_clock.tick(60)
+        dt += time_passed / 1000
 
 if __name__ == "__main__":
     main()
